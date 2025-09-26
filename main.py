5196348e36d4a3322dc2bc136a804c7c01f14d48
abhishek <developer.abhi14@gmail.com>
2025-09-26 17:24:14 +0545

    View patient data fixed

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from fastapi import FastAPI
-
+import json
 
 app = FastAPI()
 
 @app.get("/")
 def hello():
-    return {"message": "Hello World"}
+    return {"message": "Patient Management System API"}
 
 @app.get("/about")
 def about():
-    return {"message": "This is about page"}
+    return {"message": "Fully Funcional API to manage patient records"}
+
+
+def load_data():
+    with open("patients.json", "r") as f:
+        data = json.load(f)
+    return data
+
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
