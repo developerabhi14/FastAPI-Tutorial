fb5357942afb2a295b8760186b8c36679c716a53
abhishek <developer.abhi14@gmail.com>
2025-09-27 09:13:51 +0545

    Query parameters added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()
@@ -35,3 +35,21 @@ def view_patient(patient_id: str= Path(...,title="Patient ID", description="The 
         return data[patient_id]
     else:
         raise HTTPException(status_code=404, detail="Patient not found")    
+
+@app.get('/sort')
+def sort_patient(sort_by: str=Query(..., title="Sort By", description="The field to sort patients by", example="age"), order: str=Query("asc", title="Order", description="The order to sort (asc or desc)", example="asc")):
+    """Sort patients by a specific field
+    Example: Sort By:age
+    """
+    valid_field=['height','weight', 'bmi']
+    if sort_by not in valid_field:
+        raise HTTPException(status_code=400, detail=f"Invalid sort field. Valid fields are: {', '.join(valid_field)}")
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail="Invalid order. Valid orders are: asc, desc")
+    # load data
+    data=load_data()
+    # sort data
+    sort_order=True if order=='desc' else False
+    sorted_data=sorted(data.values(), key=lambda x: x.get(sort_by,0), reverse=sort_order)
+    return sorted_data
+    
